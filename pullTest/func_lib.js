07c19ea4147cdbd2ad1da378dee9e66e8eeacf2a
aSerheieva <a.serheieva@andersenlab.com>
2018-02-02 15:00:24 +0200

    add pullTest

---
+++ pullTest/func_lib.js
@@ -0,0 +1,117 @@
+//main variable
+let isResault = 'not Found';
+let isEdned = false;
+let isTimeout = false;
+
+//help function for mwWait
+function timerMy(counter) {
+    if (!isEdned && (counter > 0)) {
+        setTimeout(function () { timerMy(counter - 1) }, 1000);
+    } else {
+        isTimeout = true;
+        if (counter > 0) {
+            console.log('resault: ' + isResault);
+        } else {
+            throw new Error("timeOut_MY")
+        }
+
+    }
+}
+//fuction myWait analogue  browser.wait (but worse)
+function myWait(element, time, start=false) {
+    if (!start){
+        timerMy(time);
+    }
+    element.isEnabled().then(
+        function (val) {
+            if (!isTimeout){
+                isResault = val;
+                isEdned = true;
+                return isResault;
+            }
+        },
+        function (error) {
+                if (!isTimeout){
+                    setTimeout( function () {
+                        myWait(element, time, true);
+                    }, 1000);
+                }else {
+                    throw new Error(`${error.name} - error`);
+                }
+            }
+        )
+
+}
+
+module.exports = {
+    functionPause: function (time){
+        browser.sleep(time*1000).then(function() {
+            console.log(`waited ${time} seconds`);
+        });
+    },
+
+    goToUrl: function (url){
+        browser.get(url);
+    },
+
+    sendKeysFun: (locator, text)=>{
+
+        myWait(locator, 30000);
+        locator.clear().sendKeys(text);
+
+        browser.sleep(1000).then(function() {
+            console.log('waited 1 seconds');
+        });
+    },
+
+    clickFun: (locator)=>{
+
+        myWait(locator, 30000);
+        locator.click();
+    },
+
+
+
+};
+
+
+/*
+    changeCity: function (city){
+
+        let linkCity = element(by.css('.link.geolink[data-statlog="head.region.setup"]'));
+        myWait(linkCity, 30000);
+        linkCity.click();
+
+        let inputCity = element(by.id('city__front-input'));
+        myWait(inputCity, 30000);
+        inputCity.clear().sendKeys(city);
+
+        browser.sleep(1000).then(function() {
+            console.log('waited 1 seconds');
+        });
+
+        myWait(element(by.css('.popup.popup_animate_no.input__popup_type_geo.popup_visibility_visible')), 5000);
+        element(by.css('.b-autocomplete-item.b-autocomplete-item_type_geo:first-child')).click();
+    },
+
+    getInfoMorePopup: function (){
+
+        let moreLink = element(by.css('a.home-link.home-tabs__more-switcher'));
+        myWait(moreLink, 30000);
+        moreLink.click();
+
+        element(by.css('.popup__content .home-tabs__more')).getText().then((txt) => {
+            return txt;
+        }).catch(()=>{
+            return false;
+        });
+
+    },
+ */
+
+
+
+
+
+
+
